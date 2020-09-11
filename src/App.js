fddf09f9cecf5eea0f73609b671d5073b75fc696
Michael <michaelli.sw.eng@gmail.com>
2020-09-10 22:44:48 -0700

    Added loader and unable to fetch data error message

--- src/App.js
+++ src/App.js
@@ -15,18 +15,27 @@ class App extends Component {
         this.state = {
             dataIsLoading: true,
             data: [],
+            unableToFetchData: false,
             activeBoxDisplay: "starter"
         }
     }
 
     componentDidMount() { 
+        this.fetchData();
+    }
+
+    componentDidUpdate() {
+        this.fetchData();
+    }
+
+    fetchData = () => {
         axios.get("https://beamtech.github.io/boxing-kata-js/perks.json")
         .then((response) => {
-            this.setState({ data: response.data, dataIsLoading: false });
+            setTimeout(() => this.setState({ data: response.data, dataIsLoading: false }) , 1000);
             console.log(this.state.data);
         })
-        .catch(function (error) {
-            console.log(error);
+        .catch((error) => { 
+            this.setState({ unableToFetchData: true })
         });
     }
 
@@ -35,16 +44,28 @@ class App extends Component {
             <div id="app">
                 <div id="app-title">Shipping</div>
                 <div id="app-button-container">
-                    <Button title="Starter Boxes" type="starter" activeType={this.state.activeBoxDisplay} onClick={() => this.setState({ activeBoxDisplay: "starter" })} />
-                    <Button title="Refill Boxes" type="refill" activeType={this.state.activeBoxDisplay} onClick={() => this.setState({ activeBoxDisplay: "refill" })}/>
+                    <Button title="Starter Boxes" type="starter" activeType={this.state.activeBoxDisplay} onClick={() => this.setState({ activeBoxDisplay: "starter", dataIsLoading: true, unableToFetchData: false })} />
+                    <Button title="Refill Boxes" type="refill" activeType={this.state.activeBoxDisplay} onClick={() => this.setState({ activeBoxDisplay: "refill", dataIsLoading: true, unableToFetchData: false })}/>
                 </div>
-                <Summary activeType={this.state.activeBoxDisplay} />
-                <div id="app-card-container">
-                    <Card color="blue" brushes="2" replacementHeads="2" />
-                    <Card color="green" brushes="2" replacementHeads="2" />
-                    <Card color="pink" brushes="2" replacementHeads="2" />
-                    <Card color="blue" brushes="2" replacementHeads="2" />
-                </div>
+                 { this.state.dataIsLoading ? (
+                    <div id="app-loader-container">
+                        {!this.state.unableToFetchData ? (
+                            <div id="app-loader"></div>
+                        ) : (
+                            <div id="app-error-message">Error: Could not receive family preferences</div>
+                        )}
+                    </div>
+                ) : (
+                    <div id="app-box-info-container">
+                        <Summary activeType={this.state.activeBoxDisplay} />
+                        <div id="app-card-container">
+                            <Card color="blue" brushes="2" replacementHeads="2" />
+                            <Card color="green" brushes="2" replacementHeads="2" />
+                            <Card color="pink" brushes="2" replacementHeads="2" />
+                            <Card color="blue" brushes="2" replacementHeads="2" />
+                        </div>
+                    </div>
+                )}
             </div>
         );
     }
